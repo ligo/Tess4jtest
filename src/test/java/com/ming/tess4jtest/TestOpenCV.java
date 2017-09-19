4ededfe4464dfeb47a905dfb228b861ce4879e51
MingXing <mingxing_47@126.com>
2017-09-19 15:07:40 +0800

    Update TestOpenCV.java

--- src/test/java/com/ming/tess4jtest/TestOpenCV.java
+++ src/test/java/com/ming/tess4jtest/TestOpenCV.java
@@ -1,15 +1,4 @@
-/**
- * All rights Reserved, Designed By 云安宝
- * @Title TestOpenCV.java
- * @Package com.ming.tess4jtest
- * @Description TODO(用一句话描述该文件做什么) 
- * @author 深圳云安宝科技
- * @date 2017年7月17日 下午4:56:49
- * @version V3.0
- * @Copyright 2017 www.yunanbao.com.cn Inc. All rights reserved.
- *
- * 注意：本内容仅限于深圳云安宝科技有限公司内部传阅，禁止外泄以及用于其他商业目的 
- */
+
 package com.ming.tess4jtest;
 
 import java.io.File;
